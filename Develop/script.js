d6b7d1981769de6bc36b7f7c48a72b39cb82898d
AndreasIrta <andreasirta0711@gmail.com>
2021-03-07 21:38:29 -0700

    fix the index

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,20 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
+
 
 var password = "";
 
@@ -58,19 +75,5 @@ var charaInclusions = function() {
   return passwordInclusions;
 }
 
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
 
 
